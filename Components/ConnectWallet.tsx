6b480abbc1df730fb610b5b474c6c768afd3e4e8
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-07-19 16:25:01 +0500

    Add Connect Wallet Button

--- Components/ConnectWallet.tsx
+++ Components/ConnectWallet.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const ConnectWallet = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <button className="bg-blue-800 py-3 px-4 font-semibold rounded-md">
+            <button type="button" data-modal-target="crypto-modal" data-modal-toggle="crypto-modal" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-md px-7 py-3.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">
+                <svg aria-hidden="true" className="w-4 h-4 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
                 Connect Wallet
             </button>
         </div>
