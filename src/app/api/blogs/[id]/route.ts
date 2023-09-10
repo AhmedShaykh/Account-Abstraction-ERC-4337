917e4cefcb5fa9e8c18af8ae534b283c4b56d230
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-10 18:25:50 +0500

    Add CRUD API's

---
+++ src/app/api/blogs/[id]/route.ts
@@ -0,0 +1,108 @@
+import { NextRequest, NextResponse } from "next/server";
+import { dbConnect } from "../route";
+import prisma from "@/lib/prisma";
+
+export const GET = async (request: NextRequest) => {
+
+    try {
+
+        const id = request.url.split("/blogs/")[1];
+
+        await dbConnect();
+
+        const blogData = await prisma.post.findFirst({
+            where: {
+                id
+            }
+        });
+
+        if (!blogData) {
+
+            return NextResponse.json(
+                { message: "ID Not Found...!" }, { status: 404 }
+            );
+
+        }
+
+        return NextResponse.json(
+            { message: "ok", data: blogData }
+        );
+
+    } catch (error) {
+
+        console.log(error);
+
+    } finally {
+
+        await prisma.$disconnect();
+
+    }
+
+};
+
+export const PUT = async (request: NextRequest) => {
+
+    try {
+
+        const id = request.url.split("/blogs/")[1];
+
+        const { title, description, username } = await request.json();
+
+        await dbConnect();
+
+        const updateData = await prisma.post.update({
+            data: {
+                title,
+                description,
+                username
+            },
+            where: {
+                id
+            }
+        });
+
+        return NextResponse.json(
+            { message: "Data Updated", data: updateData }
+        );
+
+    } catch (error) {
+
+        console.log(error);
+
+    } finally {
+
+        await prisma.$disconnect();
+
+    }
+
+};
+
+export const DELETE = async (request: NextRequest) => {
+
+    try {
+
+        const id = request.url.split("/blogs/")[1];
+
+        await dbConnect();
+
+        await prisma.post.delete({
+            where: {
+                id
+            }
+        });
+
+        return NextResponse.json(
+            { message: "Data Deleted" }
+        );
+
+    } catch (error) {
+
+        console.log(error);
+
+    } finally {
+
+        await prisma.$disconnect();
+
+    }
+
+};
--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -23,9 +23,41 @@ export const GET = async (request: NextRequest) => {
 
         const blogs = await prisma.post.findMany();
 
-        return NextResponse.json({
-            message: "ok", data: blogs
-        }, { status: 200 });
+        return NextResponse.json(
+            { message: "ok", data: blogs }, { status: 200 }
+        );
+
+    } catch (error) {
+
+        console.log(error);
+
+    } finally {
+
+        await prisma.$disconnect();
+
+    }
+
+};
+
+export const POST = async (request: NextRequest) => {
+
+    try {
+
+        const { title, description, username } = await request.json();
+
+        await dbConnect();
+
+        const post = await prisma.post.create({
+            data: {
+                title,
+                description,
+                username
+            }
+        });
+
+        return NextResponse.json(
+            { message: "Data Added", data: post }, { status: 201 }
+        );
 
     } catch (error) {
 
